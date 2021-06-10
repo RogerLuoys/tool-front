833b87c176fbbc69c1a655bcc90ed2d13d0f2e90
luoys <luoys@servyou.com.cn>
2021-06-10 20:28:52 +0800

    update

--- src/main.js
+++ src/main.js
@@ -5,10 +5,11 @@ import App from './App'
 import router from './router'
 import VueAxios from 'vue-axios'
 import axios from 'axios'
-import {ElementUIComponent} from 'element-ui/types/component'
+import {ElementUI} from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.use(VueAxios, axios)
-Vue.use(ElementUIComponent)
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
