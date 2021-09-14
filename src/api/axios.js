ff6ff1111a1861301e6171d610cd5e4355060792
luoys <luoys@servyou.com.cn>
2021-09-14 19:08:12 +0800

    update

--- src/api/axios.js
+++ src/api/axios.js
@@ -8,9 +8,9 @@ const api = axios.create({
   // 办公电脑
   // baseURL: 'http://10.201.10.183:9001/',
   // 我的云主机
-  // baseURL: 'http://118.24.117.181:9001/',
+  baseURL: 'http://0.140.8.45:9011/',
   // 本地
-  baseURL: 'http://localhost:9011/',
+  // baseURL: 'http://localhost:9011/',
   timeout: 10000,
   withCredentials: false
 })
